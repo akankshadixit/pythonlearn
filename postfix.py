38ae0e9873c13e10ed2ce29abb952300fbb41ee6
Binanda <akanksha.dixit12@gmail.com>
2018-07-17 12:38:28 +0530

    postfix

---
+++ postfix.py
@@ -0,0 +1,40 @@
+from pythonds.basic.stack import Stack
+
+def infixtoPostfix(infixexp):
+   prec = {}
+   prec["*"] = 3
+   prec["/"] = 3
+   prec["+"] = 2
+   prec["-"] = 2
+   prec["("] = 1
+
+   opStack = Stack()
+   postfixList = []
+   tokenList = infixexp.split()
+
+   for token in tokenList:
+     if token in "ABCDEFGHIJKLMNOPQRSTUVQXYZ" or token in "0123456789":
+        postfixList.append(token)
+     elif token == "(":
+        opStack.push(token)
+     elif token == ")":
+        topToken = opStack.pop()
+        while topToken != "(":
+          postfixList.append(topToken)
+          topToken = opStack.pop()
+
+     else:
+       while (not opStack.isEmpty()) and (prec[opStack.peek()] >= prec[token]):
+          postfixList.append(opStack.pop())
+       opStack.push(token)
+
+   while not opStack.isEmpty():
+      postfixList.append(opStack.pop())
+   return " ".join(postfixList)
+
+
+
+print(infixtoPostfix("A * B + C * D"))
+print(infixtoPostfix("( A + B ) * C - ( D - E ) * ( F + G )"))
+
+        
